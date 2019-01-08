9403019396c8da3bdda2e16b5fae8536ef66e81c
joseph-fortman <jfortman22@gmail.com>
2019-01-08 12:27:51 -0600

    ** compile MVP **

--- compile/compile.py
+++ compile/compile.py
@@ -26,24 +26,51 @@ def compile(argv):
                 - data/stats\
                 - data/salaries")
 
-    # start loading the data into X
+    # convert PG,SG,SF,PF,C -> 1,2,3,4,5
+    positions = pd.read_csv(stats_loc, names=["Pos"], usecols=[5], dtype=str)
+    positions = positions.replace({"Pos":convert})
+
+    salary_data = salaries()
+    per_game_data = stats()
+
+    # concatenate positions and stats
+    df = pd.concat([positions,per_game_data,salary_data], axis=1)
+    # write
+    df.to_csv(matrix_loc, index=False)
+
+    r,c = df.shape
+    print("\n--Updated--\nmatrix.csv\n({},{})".format(r,c))
+
+    return 1
+# end main
+
+def salaries():
+    # read all rows except names
+    cols=[i for i in range(1,10)]
+    df = pd.read_csv(salaries_loc, usecols=cols)
+    # fill NaN with 0
+    df = df.fillna(0)
+    # create list of column headers and remove last three
+    col_list = list(df)
+    detail_cols = ['NG years','PO years','TO years']
+    for col in detail_cols:
+        col_list.remove(col)
+
+    # count rows without zeros
+    df['years left'] = (df[col_list] != 0).astype(int).sum(axis=1)
+    # sum player rows 0-5
+    df['salary left'] = df[col_list].sum(axis=1)
+    # drop yearly payment columns
+    df.drop(df.columns[[0, 1, 2, 3, 4, 5]], axis=1, inplace=True)
+
+    return df
+
+def stats():
     X = np.genfromtxt(stats_loc, delimiter=',')
     # clean data
     cols = [5,4,3,1,0]
     for col in cols:
         X = np.delete(X, col, 1)
 
-    # convert PG,SG,SF,PF,C -> 1,2,3,4,5
-    positions = pd.read_csv(stats_loc, names=["Pos"], usecols=[5], dtype=str)
-    positions = positions.replace({"Pos":convert})
-
-    # concatenate positions and stats
-    df = pd.DataFrame(X, columns=labels)
-    df = pd.concat([positions,df], axis=1)
-
-    r,c = df.shape
-    print("\n--Updated--\nmatrix.csv\n({},{})".format(r,c))
-
-    df.to_csv(matrix_loc, index=False)
-
-##end
+    return pd.DataFrame(X, columns=labels)
+# end functions
