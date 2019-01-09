f8a3aa018a0e34c9d4964d67f9c029aacd971e7b
joseph-fortman <jfortman22@gmail.com>
2019-01-09 17:50:07 -0600

    partial results vector; will move modeling to pandas (NumPy vectors--)

--- model/model.py
+++ model/model.py
@@ -2,17 +2,20 @@ import numpy as np
 from model.least_squares import calc
 import model.display
 
-training_file = '../data/matrix.csv'
-weights_file = '../data/weights.csv'
+training_file = 'data/matrix.csv'
+result_file = 'data/result.csv'
+weights_file = 'data/weights.csv'
 
 # create new classifier
 def model (argv):
 
     # load training matrix
-    X = np.genfromtxt(train_file, delimiter=',')
-    # handle file not found errors
+    X = np.genfromtxt(training_file, delimiter=',', skip_header=1)
+    print (X)
+    # handle random nan values
+    np.nan_to_num(X, copy=False)
     # load subjective results vector
-    y = np.ones((r,1))
+    y = np.genfromtxt(result_file, delimiter=',')
 
     w_hat = calc(X,y)
     print(w_hat.tostring)
@@ -22,7 +25,7 @@ def model (argv):
     fp.write(w_hat.tostring()) # doesn't work
     fp.close()
 
-    names = np.genfromtxt(train_file, dtype=('|S20'), delimiter=',', usecols=[0])
+    names = np.genfromtxt(training_file, dtype=('|S20'), delimiter=',', usecols=[0])
 
     display.display_stats(X, names)
 
