44910e8eccff5b1b6482caeff2dceda5346a59d6
joseph-fortman <jfortman22@gmail.com>
2019-01-11 08:52:28 -0600

    y vector correct shape

--- model/least_squares.py
+++ model/least_squares.py
@@ -2,21 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def display(X, names):
-    labels = ["Age", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3PA", "3P%", "2P", "2PA", "2P%", "eFG%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV", "PF", "PTS"]
-    Xdf = pd.DataFrame(X, index=names, columns=labels)
-    print(Xdf)
-
-    r,c = X.shape
-
-    fig, ax = plt.subplots()
-    for i in range(0,r):
-        ax.plot(labels, X[i,:], label=names[i])
-    ax.legend()
-    plt.show()
-
-    return
-
 
 def calc(X,y):
 
--- model/model.py
+++ model/model.py
@@ -10,16 +10,19 @@ weights_file = 'data/weights.csv'
 def model (argv):
 
     # load training matrix
-    X = np.genfromtxt(training_file, delimiter=',', skip_header=1)
-    print (X)
+    print(training_file)
+    X = np.genfromtxt('data/matrix.csv', delimiter=',', skip_header=1)
     # handle random nan values
     np.nan_to_num(X, copy=False)
-    # load subjective results vector
-    y = np.genfromtxt(result_file, delimiter=',')
+
+    # load subjective results vector and reshape to avoid (x,)
+    a,b = X.shape
+    y = np.genfromtxt(result_file, usecols=0)
+    y = np.reshape(y,(a,1))
 
     w_hat = calc(X,y)
-    print(w_hat.tostring)
 
+    print(w_hat.tostring)
     # write least squares weights
     fp = open(weights_file, 'w')
     fp.write(w_hat.tostring()) # doesn't work
