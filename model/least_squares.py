9e75484617129d12b8a299cc976f781309e9ad7f
joseph-fortman <jfortman22@gmail.com>
2019-01-18 16:17:41 -0600

    a start at cross validation

--- model/least_squares.py
+++ model/least_squares.py
@@ -4,10 +4,7 @@ import matplotlib.pyplot as plt
 import model.display as disp
 
 
-def calc(X,y):
-
-    r,c = X.shape
-
+def calc (X,y):
     # calculate eigenvectors and eignevalues
     [U,S,V] = np.linalg.svd(X, full_matrices=False)
     V = V.T
@@ -20,3 +17,24 @@ def calc(X,y):
     w_hat = V @ np.linalg.inv(np.diag(S)) @ U.T @ y
 
     return w_hat
+
+def cross_validate (X,y):
+    a,b = X.shape
+
+    sets = 10
+    group_size = a / sets
+
+    # cross validate model
+    avg_accuracy = 0;
+
+    for j in range(sets):
+        # get dataset splits
+        X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.2)
+        # least squares minimization
+        w_hat = calc(X_train, y_train)
+        y_hat = X_test * w_hat
+        total_correct = total_correct + check(y_hat,y_test);
+
+    avg_accuracy = total_correct/sets;
+
+    return avg_accuracy
